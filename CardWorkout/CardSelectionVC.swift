64bda479776907e65ffb31ace78b4f0c214cc560
veyselakbalik <akbalikveyc@gmail.com>
2023-01-04 15:56:38 +0300

    card struct created

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -12,18 +12,33 @@ class CardSelectionVC: UIViewController {
     @IBOutlet var cardImageView: UIImageView!
     
     @IBOutlet var buttons: [UIButton]!
+    var cards : [UIImage] = []
+    
+    
+    var timer: Timer!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        startTimer()
         for button in buttons {
             button.layer.cornerRadius = 8
         }
         
+        startTimer()
+        
     }
     
-
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
