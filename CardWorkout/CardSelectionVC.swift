292a583933382ba2ff7ded2388a9d256d5f56b36
veyselakbalik <akbalikveyc@gmail.com>
2023-01-04 16:13:36 +0300

    buttons connected to VC

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -12,7 +12,7 @@ class CardSelectionVC: UIViewController {
     @IBOutlet var cardImageView: UIImageView!
     
     @IBOutlet var buttons: [UIButton]!
-    var cards : [UIImage] = []
+    var cards : [UIImage] = Card.allValues
     
     
     var timer: Timer!
@@ -26,24 +26,31 @@ class CardSelectionVC: UIViewController {
             button.layer.cornerRadius = 8
         }
         
-        startTimer()
+        
         
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
     func startTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
     }
     
     @objc func showRandomImage() {
         cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
     }
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-        
+        timer.invalidate()
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
+        
     }
     
-    @IBAction func rulesButtonTapped(_ sender: UIButton) {
-    }
+    
 }
